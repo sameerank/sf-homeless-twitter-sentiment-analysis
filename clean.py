ad614a672e7378c85891cb5d0d573822d5936e19
sameerank <skunche@gmail.com>
2016-07-12 22:45:22 -0700

    now includes a script for getting tweets from twitter's API and writing them to a csv

--- clean.py
+++ clean.py
@@ -2,45 +2,44 @@ import pandas as pd
 df = pd.read_csv('sfhomeless.csv')
 
 from textblob import TextBlob
-df['polarity'] = df.apply(lambda x: TextBlob(x['Text']).sentiment.polarity, axis=1)
-df['subjectivity'] = df.apply(lambda x: TextBlob(x['Text']).sentiment.subjectivity, axis=1)
+df['polarity'] = df.apply(lambda x: TextBlob(x['text']).sentiment.polarity, axis=1)
+df['subjectivity'] = df.apply(lambda x: TextBlob(x['text']).sentiment.subjectivity, axis=1)
 
 from dateutil.parser import parse
-df['DateCreated'] = df.apply(lambda x: parse(x['DateCreated']), axis=1)
-df.sort_values(by=['DateCreated'], inplace=True)
+df['created_at'] = df.apply(lambda x: parse(x['created_at']), axis=1)
+df.sort_values(by=['created_at'], inplace=True)
 
 from difflib import SequenceMatcher
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
-from sets import Set
 cleaned_data = []
-visited = Set()
+visited = set()
 for i in range(df.shape[0]):
     if i not in visited:
         cleaned_data.append({
-        'DateCreated': df.iloc[i].DateCreated
+        'created_at': df.iloc[i].created_at
         , 'polarity': df.iloc[i].polarity
         , 'subjectivity': df.iloc[i].subjectivity
         , 'count': 0
-        , 'text': df.iloc[i].Text.replace("'","").replace('"',"").replace('\n', ' ').replace('\r', '')
+        , 'text': df.iloc[i].text.replace("'","").replace('"',"").replace('\n', ' ').replace('\r', '')
         })
     for j in range(i + 1, df.shape[0]):
-            if j not in visited and similar(df.iloc[i].Text, df.iloc[j].Text) > 0.8:
+            if j not in visited and similar(df.iloc[i].text, df.iloc[j].text) > 0.5:
                 visited.add(j)
                 cleaned_data.append({
-                    'DateCreated': df.iloc[j].DateCreated
+                    'created_at': df.iloc[j].created_at
                     , 'polarity': df.iloc[j].polarity
                     , 'subjectivity': df.iloc[j].subjectivity
                     , 'count': cleaned_data[-1]['count'] + 1
-                    , 'text': df.iloc[i].Text.replace("'","").replace('"',"").replace('\n', ' ').replace('\r', '')
+                    , 'text': df.iloc[i].text.replace("'","").replace('"',"").replace('\n', ' ').replace('\r', '')
                 })
 
 output_file = open('sfHomelessData.js', 'w')
 output_file.write('var sfHomelessData = [\n')
 
 for i in range(len(cleaned_data)):
-    new_date = cleaned_data[i]['DateCreated']
+    new_date = cleaned_data[i]['created_at']
     new_line = ('\t[new Date(' + str(new_date.year)
     + ', ' + str(new_date.month) + ', ' + str(new_date.day)
     + ', ' + str(new_date.hour)
